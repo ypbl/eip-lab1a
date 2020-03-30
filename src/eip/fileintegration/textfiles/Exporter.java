e618b2e3d466f802008ac8c977e53332d1093b6c
ernesto exposito <ernesto.exposito@univ-pau.fr>
2020-03-30 22:58:48 +0200

    This lab is intended to apply the EIP File Transfer Integration Style pattern based on the exchange of files

---
+++ src/eip/fileintegration/textfiles/Exporter.java
@@ -0,0 +1,32 @@
+package eip.fileintegration.textfiles;
+
+
+import java.io.FileNotFoundException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Exporter {
+
+    
+
+    public Exporter(String filename) throws FileNotFoundException {
+        
+    }
+
+    public void exportData(String[] data) {
+        
+    }
+    
+    public void close()
+    {
+        
+    }
+
+}
---
+++ src/eip/fileintegration/textfiles/Importer.java
@@ -0,0 +1,37 @@
+package eip.fileintegration.textfiles;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Importer {
+
+    
+
+    public Importer(String filename) throws FileNotFoundException {
+       
+    }
+
+    public String[] importData() {
+        
+        return null;
+    }
+    
+     public void close()
+    {
+        
+        
+    }
+}
